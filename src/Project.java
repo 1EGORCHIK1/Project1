6344d3bfb9cc60a4c7bce3043b9241050521a856
1EGORCHIK1 <rudinskigor@gmail.com>
2022-09-24 14:11:21 +0300

    1

--- src/Project.java
+++ src/Project.java
@@ -1,5 +1,22 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.io.*;
+
+public class Project {
+    public static void main(String[] args)
+    {
+        try(FileInputStream fin=new FileInputStream("/Users/egorchik/Downloads/Project1/src/input.txt");
+            FileOutputStream fos=new FileOutputStream("/Users/egorchik/Downloads/Project1/src/output.txt"))
+        {
+            byte[] buffer = new byte[fin.available()];
+
+            fin.read(buffer, 0, buffer.length);
+
+            fos.write(buffer, 0, buffer.length);
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+
+
     }
 }
