c645f56aa7295dc48c4b2a648b8cb743c6085d48
1EGORCHIK1 <rudinskigor@gmail.com>
2023-01-11 23:31:19 +0300

    4

--- src/Project/Project.java
+++ src/Project/Project.java
@@ -41,5 +41,70 @@ public class Project {
 
 
 
+    public void connectSocketServer(int port) {
+        try {
+            ServerSocket server = new ServerSocket(port);
+            s = server.accept();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            is = s.getInputStream();
+            os = s.getOutputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void initServer(int port){
+        while (s == null) {
+            try {
+                s = new Socket(InetAddress.getLocalHost(), port); // Подключиться к серверу
+            } catch (IOException e) {
+                //e.printStackTrace();
+            }
+        }
+
+        try {
+            os = s.getOutputStream();
+            is = s.getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    } // чтение файла из SocketClient
+
+    public void serverStop(){
+        try {
+            s.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public String getFile() {
+        byte[] b=new byte[1024];
+        try {// Определим входной поток,
+            InputStream in = is;
+            DataInputStream din = new DataInputStream (new BufferedInputStream(in)); // Создать файл для сохранения
+            filename = din.readLine();
+            flag = din.readLine();
+            File f = new File(filename);
+            RandomAccessFile fw = new RandomAccessFile(f, "rw");
+
+            int num = din.read(b);
+            while (num != -1) {// Записать 0 ~ num байтов в файл
+                fw.write(b, 0, num); // Пропустить num байтов и снова записать в файл
+                fw.skipBytes(num); // Чтение num байтов
+                num = din.read(b);
+            } // Закрыть входной и выходной потоки
+            din.close();
+            fw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return filename;
+    }
 
 }
